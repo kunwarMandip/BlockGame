b6fd754f81b385d0bd47e63633ccf2187e69a1ee
kunwarm420 <78311053+kunwarm420@users.noreply.github.com>
2024-06-23 06:22:39 +0100

    new improved constructor for enemy. needs to be updated.

--- core/src/com/mygdx/game/entity/Entity.java
+++ core/src/com/mygdx/game/entity/Entity.java
@@ -19,8 +19,10 @@ public class Entity {
     /**
      * To get this value, divide each x and y by PPM (100)
      */
-    public Vector2 bodyDimension;
-
+    private Vector2 bodyDimension;
+    private Vector2 spawnLocation;
+    private Vector2 fallingSpeed;
+    private Vector2 color;
 
 
     /**
@@ -33,6 +35,51 @@ public class Entity {
         this.bodyDimension=bodyDimension;
         defineEntity();
     }
+
+
+    /**
+     * Keep in mind all values are normalised in this constructor
+     * to my memory at least
+     * @param world the world to place bodies in
+     * @param bodyDimension height and width of the body to be created
+     * @param spawnLocation location of where the body will start from since at least x location will need to vary
+     * @param fallingSpeed some blocks may fall faster to keep the player on their toes
+     * @param color the color of the block dropping:
+     *              if y != 0, color of block may change to that.
+     *              if y==0, block will not change color and the color of that will be fallingSpeed.x
+     */
+    public Entity(World world, Vector2 bodyDimension, Vector2 spawnLocation, Vector2 fallingSpeed, Vector2 color){
+        this.world=world;
+        this.bodyDimension= new Vector2(bodyDimension.x/100, bodyDimension.y/100);
+        this.spawnLocation= new Vector2(spawnLocation.x /100, spawnLocation.y/100);
+        this.fallingSpeed=fallingSpeed;
+        this.color=color;
+        define();
+    }
+
+
+    private void define(){
+        BodyDef bodyDef  = new BodyDef();
+        bodyDef.type= BodyDef.BodyType.DynamicBody;
+        bodyDef.position.set(spawnLocation);
+        bodyDef.fixedRotation = true;
+        body = world.createBody(bodyDef);
+
+        //Create shape for the body
+        PolygonShape rectangleShape = new PolygonShape();
+        rectangleShape.setAsBox(bodyDimension.x, bodyDimension.y);
+
+        // Creates fixture definition and applies to body
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = rectangleShape;
+        fixtureDef.density = 1.0f;
+        fixtureDef.friction = 0.0f;
+        fixtureDef.restitution = 0.0f;
+
+        body.createFixture(fixtureDef);
+        rectangleShape.dispose();
+    }
+
 
     /**
      * Defines the Body to be shown in the screen
