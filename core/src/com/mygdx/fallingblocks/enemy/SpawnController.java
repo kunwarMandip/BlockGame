6d76dffb1a3707e9a8eea48a4617d480d8be23c2
kunwarm420 <78311053+kunwarm420@users.noreply.github.com>
2024-10-09 02:52:40 +0100

    Trying refacot

---
+++ core/src/com/mygdx/fallingblocks/enemy/SpawnController.java
@@ -0,0 +1,27 @@
+package com.mygdx.fallingblocks.enemy;
+
+import com.mygdx.fallingblocks.entity.enemies.Enemy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Spawner to be used exclusively for the Level
+ */
+public class SpawnController {
+
+    private final List<Enemy> enemyList= new ArrayList<>();
+
+    public SpawnController(int level){
+
+    }
+
+
+    public void draw(){
+
+    }
+
+    public void update(){
+
+    }
+}
